5f6663da6a85e01a6327e0310c36d7212e82799a
abhu66 <khoirulabhu@gmail.com>
2017-08-03 09:16:18 +0700

    Buat Fragment baru dengan nama ThridFragment

--- app/src/main/java/aplikasi4/abu/khoerul/com/samplefragmentusage/Fragment/SecondFragment.java
+++ app/src/main/java/aplikasi4/abu/khoerul/com/samplefragmentusage/Fragment/SecondFragment.java
@@ -1,19 +1,19 @@
 package aplikasi4.abu.khoerul.com.samplefragmentusage.Fragment;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import aplikasi4.abu.khoerul.com.samplefragmentusage.R;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SecondFragment extends Fragment {
 
+    private Button btnSecond;
 
     public SecondFragment() {
         // Required empty public constructor
@@ -24,7 +24,37 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        btnSecond = (Button)view.findViewById(R.id.btn_second_fragment);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Second Fragment");
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setHasOptionsMenu(true);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().
+                        replace(R.id.frame_container,
+                                new ThirdFragment(),
+                                ThirdFragment.class.getSimpleName())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            getFragmentManager().popBackStack();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
