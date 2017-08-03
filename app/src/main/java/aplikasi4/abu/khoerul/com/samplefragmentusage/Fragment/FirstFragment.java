9f8a37f8d3bcbaa2cfe87904506348c7adf9f630
abhu66 <khoirulabhu@gmail.com>
2017-08-03 09:10:02 +0700

    modif isi FirstFragment.java

--- app/src/main/java/aplikasi4/abu/khoerul/com/samplefragmentusage/Fragment/FirstFragment.java
+++ app/src/main/java/aplikasi4/abu/khoerul/com/samplefragmentusage/Fragment/FirstFragment.java
@@ -3,17 +3,17 @@ package aplikasi4.abu.khoerul.com.samplefragmentusage.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import aplikasi4.abu.khoerul.com.samplefragmentusage.R;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FirstFragment extends Fragment {
-
+    private Button btnFirstFragment;
 
     public FirstFragment() {
         // Required empty public constructor
@@ -23,8 +23,27 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view =  inflater.inflate(R.layout.fragment_first, container, false);
+        btnFirstFragment = (Button)view.findViewById(R.id.btn_first_fragment);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("First Fragment");
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        btnFirstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().
+                        setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).
+                        replace(R.id.frame_container,
+                                new SecondFragment(),
+                                SecondFragment.class.getSimpleName())
+                        .addToBackStack(null).commit();
+            }
+        });
+    }
 }
