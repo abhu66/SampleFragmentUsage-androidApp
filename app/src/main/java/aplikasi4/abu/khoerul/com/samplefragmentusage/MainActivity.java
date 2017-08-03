ebfaf5624146889d9f28f48de8502024b4fd57ee
abhu66 <khoirulabhu@gmail.com>
2017-08-03 09:04:31 +0700

    comit pertama

---
+++ app/src/main/java/aplikasi4/abu/khoerul/com/samplefragmentusage/MainActivity.java
@@ -0,0 +1,13 @@
+package aplikasi4.abu.khoerul.com.samplefragmentusage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
