355957598cd50ed77f5faade81148584c8abccbc
abhu66 <khoirulabhu@gmail.com>
2017-08-03 09:18:34 +0700

    Modif isi ThirdFragment.java

--- app/src/main/java/aplikasi4/abu/khoerul/com/samplefragmentusage/Fragment/ThirdFragment.java
+++ app/src/main/java/aplikasi4/abu/khoerul/com/samplefragmentusage/Fragment/ThirdFragment.java
@@ -4,10 +4,9 @@ package aplikasi4.abu.khoerul.com.samplefragmentusage.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import aplikasi4.abu.khoerul.com.samplefragmentusage.R;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +23,18 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("ThirdFragment");
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setHasOptionsMenu(true);
+        return view;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            getFragmentManager().popBackStack();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
